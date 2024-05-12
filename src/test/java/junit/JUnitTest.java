57479ceaefb6fe4bae61b535b79b04d57b61e671
Nicko Shvetsov <shvetsovnick@gmail.com>
2024-05-12 16:58:33 -0500

    junit package added, JUnitTest.java added

---
+++ src/test/java/junit/JUnitTest.java
@@ -0,0 +1,22 @@
+package junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+    @BeforeEach
+    void beforeEach(){
+
+    }
+    @AfterEach
+    void afterEach(){
+
+    }
+
+
+    @Test
+    void firstTest() {
+
+    }
+}
